7324fb1acea5f29a48efc9fcfb6a781e1fa353df
robsidebotham <rob_sidebotham@hotmail.co.uk>
2020-06-10 21:33:18 +0100

    Adds job post component.

--- components/HeaderComponent.js
+++ components/HeaderComponent.js
@@ -16,15 +16,15 @@ function HeaderComponent({ name }) {
 const styles = {
   container: {
     backgroundColor: '#9FEDD7',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
   },
   left: {
     color: '#026670',
     fontSize: 20,
     fontWeight: 'bold',
     width: 500,
-    paddingRight: 200
-  }
+    paddingRight: 200,
+  },
 };
 
 export default HeaderComponent;
--- components/JobAdder.js
+++ components/JobAdder.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import {Input} from 'react-native-elements';
 
 export default function JobAdder() {
   return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Input placeholder={'Title'} label={'Job Title'}/>
-        <Input placeholder={'Info'} label={'Job Info'} />
+      <View style={{ flex: 1 }}>
+        <TextInput placeholder={'Title'} style={styles.inputBox}/>
+        <TextInput
+        multiline={true}
+        numberOfLines={4}
+        placeholder={'Job Info'}
+        style={styles.inputBox}
+      />
+      <TextInput placeholder={'Skills'} style={styles.inputBox} />
+      <TouchableOpacity onPress={() => console.log('This will call a func that adds job')}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Post Job</Text>
+        </View>
+        </TouchableOpacity>
       </View>
   )
 }
+const styles = {
+  button: {
+    backgroundColor: '#026670',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+    padding: 6,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 26,
+  },
+  inputBox: {
+    height: 100,
+    margin: 3,
+    backgroundColor: '#fff',
+    fontColor: '#026670',
+    fontSize: 26,
+  },
+};
