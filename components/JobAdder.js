a0f2364302d1cab23c6c04aa17bc4bf6b6538cfe
Seb Smith <seb-smith@live.com>
2020-06-11 11:05:43 +0100

    Adds styling to post a job form and a drop down box

--- components/JobAdder.js
+++ components/JobAdder.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, TextInput } from 'react-native';
-import {Input} from 'react-native-elements';
+import { Input } from 'react-native-elements';
+import UserContext from '../contexts/UserContext';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function JobAdder() {
+  const [jobInfo, setJobInfo] = useState({
+    title: 'hello world',
+    body: 'foo bar',
+    username: UserContext._currentValue,
+    skill_name: 'computers',
+    location: 'M12',
+  });
+
   return (
-      <View style={{ flex: 1 }}>
-        <TextInput placeholder={'Title'} style={styles.inputBox}/>
-        <TextInput
+    <View style={{ flex: 1 }}>
+      <Text style={styles.inputHeading}>{'Title'}</Text>
+      <TextInput style={styles.inputBox} />
+      <Text style={styles.inputHeading}>{'Description'}</Text>
+      <TextInput
         multiline={true}
         numberOfLines={4}
-        placeholder={'Job Info'}
         style={styles.inputBox}
+        //on change update state with info from inside the box
       />
-      <TextInput placeholder={'Skills'} style={styles.inputBox} />
-      <TouchableOpacity onPress={() => console.log('This will call a func that adds job')}>
+      <Text style={styles.inputHeading}>{'Skills'}</Text>
+      <DropDownPicker
+        items={[
+          { label: 'computers', value: 'computers' },
+          { label: 'gardening', value: 'gardening' },
+        ]}
+        style={{ backgroundColor: '#fafafa' }}
+        dropDownStyle={{ backgroundColor: '#fafafa' }}
+      />
+      <Text style={styles.inputHeading}>{'Location'}</Text>
+      <TextInput style={styles.inputBox} />
+      <TouchableOpacity
+        onPress={() =>
+          console.log('sends a post request to the api with state')
+        }
+      >
         <View style={styles.button}>
           <Text style={styles.buttonText}>Post Job</Text>
         </View>
-        </TouchableOpacity>
-      </View>
-  )
+      </TouchableOpacity>
+    </View>
+  );
 }
+
 const styles = {
   button: {
     backgroundColor: '#026670',
@@ -37,10 +64,20 @@ const styles = {
     fontSize: 26,
   },
   inputBox: {
-    height: 100,
-    margin: 3,
+    borderBottomWidth: 2,
+    borderBottomColor: '#FCE181',
+    padding: 4,
     backgroundColor: '#fff',
-    fontColor: '#026670',
-    fontSize: 26,
+    fontSize: 14,
+    color: '#026670',
+  },
+  inputHeading: {
+    fontSize: 20,
+    marginTop: 20,
+    paddingLeft: 10,
+    color: '#026670',
+    backgroundColor: '#FEF9C7',
+    borderTopWidth: 2,
+    borderTopColor: '#FCE181',
   },
 };
--- screens/AddJobScreen.js
+++ screens/AddJobScreen.js
@@ -1,16 +1,11 @@
-import React from "react";
-import { View, Text } from "react-native";
-import HeaderComponent from "../components/HeaderComponent";
+import React from 'react';
+import { View, Text } from 'react-native';
+import HeaderComponent from '../components/HeaderComponent';
 import JobAdder from '../components/JobAdder';
-import UserContext from '../contexts/UserContext';
-
 
 const AddJobScreen = () => {
   return (
     <>
-      <UserContext.Consumer>
-        {(value) => console.log(value)}
-      </UserContext.Consumer>
       <HeaderComponent name="Add Job" />
       <JobAdder />
     </>
