c4972778136f0c1daec8f40917e2e60498af717a
James Oliver <joliver.robert@me.com>
2020-06-15 16:47:45 +0100

    adds UserAdder component

---
+++ components/UserAdder.js
@@ -0,0 +1,201 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import * as api from '../api-requests/axios-request';
+import ImagePickerComponent from '../utils/imagePicker';
+import { userSignUp } from '../api-requests/sign-up-user';
+
+export default function UserAdder({ toggleOverlay, setLoading }) {
+  const [newUserInfo, setNewUserInfo] = useState({
+    email: '',
+    password: '',
+    username: '',
+    first_name: '',
+    last_name: '',
+    skill_name: '',
+    location: '',
+    bio: '',
+    charity_name: '',
+  });
+  const [image, setImage] = useState(null);
+  const [skills, setSkills] = useState([]);
+  const [charities, setCharities] = useState([]);
+
+  useEffect(() => {
+    api
+      .getSkills()
+      .then(({ skills }) => {
+        setSkills(
+          skills.map(skill => {
+            return { label: skill.skill_name, value: skill.skill_name };
+          })
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    api
+      .getCharities()
+      .then(({ charities }) => {
+        setCharities(
+          charities.map(charity => {
+            return { label: charity.charity_name, value: charity.charity_name };
+          })
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleUserPost = () => {
+    setLoading(true);
+
+    userSignUp(newUserInfo, image)
+      .then(() => {
+        setLoading(false);
+        toggleOverlay();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleTextChange = (text, key) => {
+    let updatedUserInfo = {};
+    Object.assign(updatedUserInfo, newUserInfo);
+    updatedUserInfo[key] = text;
+    setNewUserInfo(updatedUserInfo);
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#e4f5f0' }}>
+      <ScrollView>
+        <Text style={styles.inputHeading}>{'Username'}</Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => handleTextChange(text, 'username')}
+        />
+        <Text style={styles.inputHeading}>{'First Name'}</Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => handleTextChange(text, 'first_name')}
+        />
+        <Text style={styles.inputHeading}>{'Last Name'}</Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => handleTextChange(text, 'last_name')}
+        />
+        <Text style={styles.inputHeading}>{'email'}</Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => handleTextChange(text, 'email')}
+        />
+        <Text style={styles.inputHeading}>{'password'}</Text>
+        <TextInput
+          secureTextEntry={true}
+          style={styles.inputBox}
+          onChangeText={text => handleTextChange(text, 'password')}
+        />
+
+        <View style={styles.button}>
+          <ImagePickerComponent setImageObj={setImage}></ImagePickerComponent>
+        </View>
+
+        <Text style={styles.inputHeading}>{'Location'}</Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => handleTextChange(text, 'location')}
+        />
+        <Text style={styles.inputHeading}>{'Bio'}</Text>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => handleTextChange(text, 'bio')}
+        />
+        <Text style={styles.inputHeading}>{'Charity Name'}</Text>
+        <DropDownPicker
+          items={charities}
+          style={styles.dropDownPicker}
+          dropDownStyle={styles.dropDownPickerDropDown}
+          containerStyle={styles.dropDownContainer}
+          activeItemStyle={styles.dropDownPickerItem}
+          onChangeItem={item => handleTextChange(item.label, 'charity_name')}
+        />
+        <Text style={styles.inputHeading}>{'Skills'}</Text>
+        <DropDownPicker
+          items={skills}
+          style={styles.dropDownPicker}
+          dropDownStyle={styles.dropDownPickerDropDown}
+          containerStyle={styles.dropDownContainer}
+          activeItemStyle={styles.dropDownPickerItem}
+          onChangeItem={item => handleTextChange(item.label, 'skill_name')}
+        />
+        <TouchableOpacity onPress={() => handleUserPost(newUserInfo)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Sign Up!</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = {
+  button: {
+    backgroundColor: '#026670',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+    padding: 6,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 26,
+  },
+  inputBox: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#FCE181',
+    padding: 4,
+    backgroundColor: '#fff',
+    fontSize: 14,
+    color: '#026670',
+    height: 40,
+  },
+  inputHeading: {
+    fontSize: 20,
+    marginTop: 10,
+    paddingLeft: 10,
+    color: '#026670',
+    backgroundColor: '#FEF9C7',
+    borderTopWidth: 2,
+    borderTopColor: '#FCE181',
+  },
+  dropDownPicker: {
+    backgroundColor: '#fff',
+    borderBottomWidth: 2,
+    borderBottomColor: '#FCE181',
+  },
+  dropDownPickerDropDown: {
+    backgroundColor: '#fff',
+  },
+  dropDownPickerItem: {
+    fontSize: 14,
+    color: '#026670',
+  },
+  dropDownContainer: {
+    height: 50,
+  },
+  dropDownContainer: {
+    height: 50,
+  },
+};
